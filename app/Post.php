bbf1ecc96059735967a56592f6caad732ab37d52
Recep GUCLUER <rgucluer@gmail.com>
2018-02-28 14:36:46 +0300

    Tutorial "Laravel from scratch: Ep. 21: View Composers"  finished and applied to the project.

--- app/Post.php
+++ app/Post.php
@@ -44,4 +44,13 @@ class Post extends Model
             }
         }
     } // public function scopeFilter
+    
+    public static function archives()
+    {
+        return static::selectRaw('year(created_at) year, monthname(created_at) month, count(*) published')
+            ->groupBy('year', 'month')
+            ->orderByRaw('min(created_at) desc')
+            ->get()
+            ->toArray();
+    }
 }
