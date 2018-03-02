234a4792e02c77e1f74a1f2bc124bc8d5785d861
Recep GUCLUER <rgucluer@gmail.com>
2018-03-02 16:17:38 +0300

    Tutorial "Laravel from scratch: Ep. 22: Testing 101"  finished and applied to the project.

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,7 +3,10 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
@@ -14,8 +17,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $this->get('/')->assertSee('DevsBlog');
+        
     }
 }
