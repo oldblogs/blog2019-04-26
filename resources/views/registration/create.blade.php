f3b3aaf3c8a674fc9d7612d0b892d5ec5593aa9b
Recep GUCLUER <rgucluer@gmail.com>
2018-03-04 16:01:24 +0300

    Made modifications in the form code to remember previously entered values.

--- resources/views/registration/create.blade.php
+++ resources/views/registration/create.blade.php
@@ -8,12 +8,12 @@
       
       <div class="form-group">
         <label for="name">Name:</label>
-        <input type="text" class="form-control" id="name" name="name" required>
+        <input type="text" class="form-control" id="name" name="name" value="{{ old( 'name' ) }}" required>
       </div>
       
       <div class="form-group">
         <label for="email">Email:</label>
-        <input type="email" class="form-control" id="email" name="email" required>
+        <input type="email" class="form-control" id="email" name="email" value="{{ old( 'email' ) }}" required>
       </div>
       
       <div class="form-group">
