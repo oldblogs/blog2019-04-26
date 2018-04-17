9ffd3c1b0090c20c7a3aa4eabf75c27f8a529720
Recep GUCLUER <rgucluer@gmail.com>
2018-04-17 12:10:49 +0300

    ------
    Read Security / API Authentication,  and partly implemented.
    https://laravel.com/docs/5.6/passport
    
    ------
    
    php7.1-gmp
    
    installed to development system by ubuntu package manager
    
    ------
    
    laravel/passport
    mdanter/ecc
    curl/curl
    
    installed by composer
    
    ------
    
    Installed passport by artisan
    
    Added the Laravel\Passport\HasApiTokens trait to our App\User model.
    
    Called the Passport::routes method within the boot method of our AuthServiceProvider.
    
    In our config/auth.php configuration file, set the driver option of
      the api authentication guard to passport.
    
    php artisan vendor:publish --tag=passport-components
    
    Register these components in resources/assets/js/app.js .
    
    Recompiled with
    
    npm install
    npm run dev
    
    php artisan passport:keys
    
    ------
    
    Full cycle of authorizing clients not implemented.
    
    ------
    
    Added the CreateFreshApiToken middleware to our web middleware group in
    app/Http/Kernel.php file:
    
    ```
    'web' => [
        // Other middleware...
        \Laravel\Passport\Http\Middleware\CreateFreshApiToken::class,
    ],
    ```

---
+++ resources/assets/js/components/VueTest.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="col-xs-12">
+        <div class="card card-default">
+            <div class="card-header">Vue Test</div>
+
+            <div class="card-body">
+              Testing AJAX
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.');
+            window.axios.get('/api/user')
+              .then(response => {
+                  console.log(response.data);
+              });
+        }
+    }
+</script>
