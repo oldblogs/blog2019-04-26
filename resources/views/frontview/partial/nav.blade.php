dba48d610900b18eab5d5af88dc4f872d9a934fc
Recep GUCLUER <rgucluer@gmail.com>
2023-07-08 01:15:11 +0300

    Comment out front view Subscribe, and Search icon.

--- resources/views/frontview/partial/nav.blade.php
+++ resources/views/frontview/partial/nav.blade.php
@@ -2,14 +2,14 @@
     <header class="blog-header py-3">
       <div class="row flex-nowrap justify-content-between align-items-center">
         <div class="col-4 pt-1">
-          <a class="text-muted" href="#">Subscribe</a>
+          {{-- <a class="text-muted" href="#">Subscribe</a> --}}
         </div>
         <div class="col-4 text-center">
           <a class="blog-header-logo text-dark" href="#">Large</a>
         </div>
         <div class="col-4 d-flex justify-content-end align-items-center">
           <a class="nav-link text-muted" href="#">
-          <i class="fa-solid fa-magnifying-glass"></i>
+          {{-- <i class="fa-solid fa-magnifying-glass"></i> --}}
           </a>
         </div>
       </div>
