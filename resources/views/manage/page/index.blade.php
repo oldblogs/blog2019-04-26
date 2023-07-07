ccafa49bd262c60a4be9b6d09a64d6e147c2da8e
Recep GUCLUER <rgucluer@gmail.com>
2023-07-08 00:53:38 +0300

    Seperate manage content blocks.

--- resources/views/manage/page/index.blade.php
+++ resources/views/manage/page/index.blade.php
@@ -8,10 +8,12 @@
   <div class="d-flex justify-content-between flex-wrap align-items-center pb-2 mb-3 border-bottom">
     <h1 class="h2">Dashboard</h1>
   </div>
-
+  <p>TODO: Add website metrics here.</p>
+  <br>
   <posts></posts>
-
+  <br>
   <media></media>
+  <br>
 
   <div class="d-flex justify-content-between flex-wrap align-items-center pb-2 mb-3 border-bottom">
     <h1 class="h2">OAuth</h1>
