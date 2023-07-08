fa351c40009a397ff5fb461e713f4d873870144f
Recep GUCLUER <rgucluer@gmail.com>
2023-07-09 01:19:58 +0300

    Add margin bottom to profile image in front view.

--- resources/views/frontview/partial/aboutrow.blade.php
+++ resources/views/frontview/partial/aboutrow.blade.php
@@ -1,7 +1,7 @@
 <div class="col-sm-12 col-md-12 col-lg-6">
   <div class="card mb-3">
     <div class="card-body">
-      <img class="card-img-top img-thumbnail" style="max-height: 6rem; max-width: 6rem;" src="{{ asset('img/profile.png') }}" alt="Photo of user" height="60" width="60"> 
+      <img class="card-img-top img-thumbnail mb-3" style="max-height: 6rem; max-width: 6rem;" src="{{ asset('img/profile.png') }}" alt="Photo of user" height="60" width="60"> 
       <h3 class="card-title">{{ $about->title }}</h5>
       <h4 class="card-subtitle mb-2 text-muted">{{ $about->subtitle }}</h6>
       <p class="card-text">{{ $about->body }}</p>
