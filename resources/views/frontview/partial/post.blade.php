aaf6af1bb554052cc96dce63659a41afbbb68b5d
Recep GUCLUER <rgucluer@gmail.com>
2023-07-20 12:48:32 +0300

    Add writers name, photo, date, time on blog post listing on home page

--- resources/views/frontview/partial/post.blade.php
+++ resources/views/frontview/partial/post.blade.php
@@ -16,7 +16,7 @@
         $dt->format('l jS \\of F Y h:i:s A');  // Thursday, 25th of December 1975 02:15:16 PM
       --}}
     <p class="card-text mb-auto">{{ substr($post->body,0,400) }}</p>
-    
+    @include ('frontview.partial.postauthor')
     <a href="/posts/{{ $post->id }}">Continue reading</a>
   </div>
 </div>
