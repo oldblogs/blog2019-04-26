8e732d6280803d781f4522f5ae94b9eb79cda30c
Recep GUCLUER <rgucluer@gmail.com>
2023-07-20 12:48:32 +0300

    Add writers name, photo, date, time on blog post listing on home page

--- resources/views/frontview/page/posts/index.blade.php
+++ resources/views/frontview/page/posts/index.blade.php
@@ -13,6 +13,8 @@
         <div class="col-md-6 px-0">
           <h1 class="display-4 font-italic">{{ $post->title }}</h1>
           <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.</p>
+          @include ('frontview.partial.postauthor')
+          <p>{{ $post->created_at->toFormattedDateString() }}</p>
           <p class="lead mb-0"><a href="/posts/{{ $post->id }}" class="text-white font-weight-bold">Continue reading...</a></p>
         </div>
       </div>
--- resources/views/frontview/partial/aboutrow.blade.php
+++ resources/views/frontview/partial/aboutrow.blade.php
@@ -2,8 +2,8 @@
   <div class="card mb-3">
     <div class="card-body">
       <img class="card-img-top img-thumbnail mb-3" style="max-height: 6rem; max-width: 6rem;" src="{{ asset('storage/'.$about->photo) }}" alt="Photo of user" height="60" width="60"> 
-      <h3 class="card-title">{{ $about->title }}</h5>
-      <h4 class="card-subtitle mb-2 text-muted">{{ $about->subtitle }}</h6>
+      <h3 class="card-title">{{ $about->title }}</h3>
+      <h4 class="card-subtitle mb-2 text-muted">{{ $about->subtitle }}</h4>
       <p class="card-text">{{ $about->body }}</p>
       <h5 class="card-title">Contact information:</h5>
       <ul class="list-group list-group-flush">
