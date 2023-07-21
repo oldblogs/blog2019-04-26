b465ca1a71c45f1cbd93638bdee2c1ce4c3bb5d4
Recep GUCLUER <rgucluer@gmail.com>
2023-07-21 13:50:29 +0300

    Fix css of post views.

--- resources/views/frontview/page/mainpage.blade.php
+++ resources/views/frontview/page/mainpage.blade.php
@@ -12,7 +12,7 @@
       <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
         <div class="col-md-6 px-0">
           <h1 class="display-4 font-italic">{{ $post->title }}</h1>
-          <p class="lead my-3">{{ substr($post->body,0,400) }}</p>
+          <p class="lead my-3">{{ substr($post->body,0,200) }} ...</p>
           @include ('frontview.partial.postauthor')
           <p>{{ $post->created_at->toFormattedDateString() }}</p>
           <p class="lead mb-0"><a href="/posts/{{ $post->id }}" class="text-white font-weight-bold">Continue reading...</a></p>
