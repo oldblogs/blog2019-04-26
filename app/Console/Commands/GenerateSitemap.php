cf82ac2c96d6db93cc8e4698c5336dfa67818119
Recep GUCLUER <rgucluer@gmail.com>
2023-07-25 01:59:28 +0300

    Install laravel-sitemap
    Add laravel-sitemap to project. Create an artisan command sitemap:generate.
    Error, command does not generate sitemap.xml at this time.

---
+++ app/Console/Commands/GenerateSitemap.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Spatie\Sitemap\SitemapGenerator;
+use Carbon\Carbon;
+
+class GenerateSitemap extends Command
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $signature = 'sitemap:generate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate the sitemap.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        try{
+            echo (public_path('sitemap.xml'));
+            SitemapGenerator::create(config('app.url'))
+                ->writeToFile(public_path('sitemap.xml'));
+        }
+        catch(Exception $e){
+            $this->error('Error : '.$e->message() );
+            exit();
+        }
+
+    }
+}
