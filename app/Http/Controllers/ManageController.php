22d0c1f8814e362c5476e5dd3f665afe2dca1fed
Recep GUCLUER <rgucluer@gmail.com>
2023-07-26 16:37:32 +0300

    Remove idle controller functions.

--- app/Http/Controllers/ManageController.php
+++ app/Http/Controllers/ManageController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Repositories\PostsRepository;
+
 use Carbon\Carbon;
 use Illuminate\Session\SessionManager;
 use App\Http\Requests\PostForm;
@@ -13,18 +13,7 @@ class ManageController extends Controller
     public function __construct() {
         $this->middleware('auth');
     }
-    
-    // TODO: Delete after conversion
-    public function postslist(PostsRepository $postsrepo){
-        $posts = $postsrepo->paginated();
-        return view('manage.page.posts.index', compact('posts') );
-    }
-    
-    // TODO: Delete after conversion
-    public function viewpost(Post $post){
-        return view('manage.page.posts.show', compact('post') );
-    }
-    
+
     // TODO: Delete before production
     public function test(User $user){
         return view('manage.page.test', compact('user') );
